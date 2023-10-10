f6a144295affdded5ac96ef219c9da9e4c3d72bd
Toma Milkov <tmilkov@appolica.com>
2023-10-10 14:27:37 +0200

    Added configuration for the tests.

--- src/test/java/com/fontysxmaua/UniGuide/UniGuideApplicationTests.java
+++ src/test/java/com/fontysxmaua/UniGuide/UniGuideApplicationTests.java
@@ -2,10 +2,16 @@ package com.fontysxmaua.UniGuide;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class UniGuideApplicationTests {
 
+	@Configuration
+	static class TestConfig {
+		// Define beans or configuration settings here
+	}
+	
 	@Test
 	void contextLoads() {
 	}

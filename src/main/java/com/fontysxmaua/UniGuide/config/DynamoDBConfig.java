21b3e6ba05b220d77637a999c81bebda3a41b751
Toma Milkov <tmilkov@appolica.com>
2023-10-24 22:29:48 +0200

    Fixed Lombok issues.

--- src/main/java/com/fontysxmaua/UniGuide/config/DynamoDBConfig.java
+++ src/main/java/com/fontysxmaua/UniGuide/config/DynamoDBConfig.java
@@ -4,11 +4,11 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import io.micrometer.common.util.StringUtils;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableDynamoDBRepositories
--- src/main/java/com/fontysxmaua/UniGuide/model/request/ActivityRequest.java
+++ src/main/java/com/fontysxmaua/UniGuide/model/request/ActivityRequest.java
@@ -1,13 +1,13 @@
 package com.fontysxmaua.UniGuide.model.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data

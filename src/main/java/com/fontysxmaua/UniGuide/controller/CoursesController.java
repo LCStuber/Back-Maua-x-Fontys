a7202ab1f289e125dd09abad87068e88dcc3d925
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-10-31 11:39:46 -0300

    Create backend for Courses

---
+++ src/main/java/com/fontysxmaua/UniGuide/controller/CoursesController.java
@@ -0,0 +1,42 @@
+package com.fontysxmaua.UniGuide.controller;
+
+import com.fontysxmaua.UniGuide.model.request.CourseRequest;
+import com.fontysxmaua.UniGuide.model.response.CourseResponse;
+import com.fontysxmaua.UniGuide.service.CoursesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+@RequiredArgsConstructor
+public class CoursesController {
+
+    private final CoursesService service;
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> getAllCourses() {
+        return ResponseEntity.ok(service.getAllCourses());
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseResponse> addCourse(
+            @RequestBody @Valid CourseRequest request) {
+        return ResponseEntity.ok(service.addCourse(request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable String id) {
+        service.deleteCourse(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CourseResponse> updateCourse(@PathVariable String id,
+            @RequestBody @Valid CourseRequest request) {
+        return ResponseEntity.ok(service.updatCourse(id,request));
+    }
+}
---
+++ src/main/java/com/fontysxmaua/UniGuide/mapper/CourseMapper.java
@@ -0,0 +1,26 @@
+package com.fontysxmaua.UniGuide.mapper;
+
+import com.fontysxmaua.UniGuide.model.entity.Course;
+import com.fontysxmaua.UniGuide.model.request.CourseRequest;
+import com.fontysxmaua.UniGuide.model.response.CourseResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+    public CourseResponse toResponse(Course course) {
+        return new CourseResponse(
+                course.getId(),
+                course.getName(),
+                course.getDescription(),
+                course.getCoordinator()
+        );
+    }
+
+    public Course toEntity(CourseRequest request) {
+        return Course.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .coordinator(request.getCoordinator())
+                .build();
+    }
+}

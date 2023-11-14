3d896e6e4e906cd9011d193601afb089404a3e55
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-11-13 22:54:38 -0300

    Implementation: University Info

---
+++ src/main/java/com/fontysxmaua/UniGuide/mapper/UniversityInfoMapper.java
@@ -0,0 +1,25 @@
+package com.fontysxmaua.UniGuide.mapper;
+
+import com.fontysxmaua.UniGuide.model.entity.UniversityInfo;
+import com.fontysxmaua.UniGuide.model.request.UniversityInfoRequest;
+import com.fontysxmaua.UniGuide.model.response.UniversityInfoResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniversityInfoMapper {
+    public UniversityInfoResponse toResponse(UniversityInfo universityInfo) {
+        return new UniversityInfoResponse(
+                universityInfo.getId(),
+                universityInfo.getRector(),
+                universityInfo.getViceRector()
+        );
+    }
+
+    public UniversityInfo toEntity(UniversityInfoRequest request) {
+        return UniversityInfo.builder()
+                .rector(request.getRector())
+                .viceRector(request.getViceRector())
+                .build();
+    }
+}
